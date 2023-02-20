6cf87499673fcec54f5b3e3871060b18f1e5d684
ChouCookieSan <98222012+CookieSan64@users.noreply.github.com>
2023-02-20 09:21:24 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config                           = {}
+Config.DrawDistance              = 100.0
+Config.MarkerColor               = {r = 39, g = 131, b = 206}
+
+Config.Zones = {
+	VoteBooth1 = {
+		MarkerPos  = {x = -537.7205, y = -178.3969, z = 37.22707 },
+		Size       = {x = 1.5, y = 1.5, z = 1.0},
+		Type       = 27
+	}
+}
